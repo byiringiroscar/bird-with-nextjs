14e4a215089a99ee92572d30ac9316e5959e1352
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 09:25:08 +0200

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'Features', label: 'Features' },
+    { href: '/', key: 'About', label: 'About' },
+    { href: '/', key: 'help ', label: 'Help Center & Community ' },
+  ];
