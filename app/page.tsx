e5d61f020c04b2bec2456d579d374e8a671fa0c1
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 09:22:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
+      <h2>hey</h2>
     </main>
   );
 }
