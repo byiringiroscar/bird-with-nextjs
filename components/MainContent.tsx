78f53467aad6bdcf5c5155778689b2d33f06fe83
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 12:51:47 +0200

    Update MainContent.tsx

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -5,7 +5,12 @@ import Image from 'next/image';
 
 const MainContent = () => {
   return (
-    <div className='w-full relative'>
+    <div className='w-full relative flex flex-col items-center py-8'>
+
+        <p className='border text-[11px] tracking-[0.1px] font-semibold text-[#858585] px-7 py-[5px] rounded-[43px] border-solid border-[#b1aeae] cursor-pointer'>Organization</p>
+
+
+
         <Image 
             src={leftSide}
             alt='left side'
