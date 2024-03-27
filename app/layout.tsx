0ec7de8d8a603a7ff6fc4b91a590f02041206bb9
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 16:10:10 +0200

    update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/logo.png" />
+
+      </head>
       <body  className='flex flex-col min-h-screen'>
           <Navbar />
             <main className='relative overflow-hidden flex-grow'>{children}</main>
