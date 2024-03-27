c44b91a3edb7cc1698f891855da3f8bbd4b6375b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 12:45:21 +0200

    wip on img

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import leftSide from '../public/left-side.png';
+import rightSide from '../public/right-side.png';
+import Image from 'next/image';
 
 const MainContent = () => {
   return (
-    <div>MainContent</div>
+    <div className='w-full relative'>
+        <Image 
+            src={leftSide}
+            alt='left side'
+            className='leftSideImage'
+        />
+        <Image 
+            src={rightSide}
+            alt='right side'
+            className='rightSideImage'
+        />
+
+    </div>
   )
 }
 
