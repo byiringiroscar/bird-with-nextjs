c12d448dd97f0d860688d584dc38348c529ccac4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 09:26:17 +0200

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'Features', label: 'Features' },
-    { href: '/', key: 'About', label: 'About' },
-    { href: '/', key: 'help ', label: 'Help Center & Community ' },
+    { href: '/', key: 'deveopers', label: 'Developers' },
+    { href: '/', key: 'organizations', label: 'Organizations' },
+    { href: '/', key: 'login', label: 'Login' },
   ];
