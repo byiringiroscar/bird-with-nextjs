b3700416674a6439557ee39debfdc064480aaae5
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 13:19:16 +0200

    wip in input

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -9,10 +9,13 @@ const MainContent = () => {
 
         <p className='border text-[11px] tracking-[0.1px] font-semibold text-[#858585] px-7 py-[5px] rounded-[43px] border-solid border-[#b1aeae] cursor-pointer opensans-font-regular'>Organization</p>
         <h1 className='text-center text-[41px] leading-[50px] mt-[25px] mb-2.5 pt-0 pb-3 px-0 montserrat-700'><span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>Evaluate Candidates</span> Quickly, <br /> Affordably, and Accurately</h1>
-          <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center'>
             <p className='text-center leading-[27px] opacity-70 text-base montserrat-400'>The #1 platform for technical assessments, interviews, and projects.</p>
             <p className='text-center leading-[27px] opacity-70 text-base montserrat-400'>Get unlimited candidates and admins on every plan.</p>
-          </div>
+        </div>
+        <div className="trial-form">
+            <input type="email" placeholder="Work email address" className="mainEmail"></input> <button className="mainTopButton">Start Trial</button>
+        </div>
 
 
         <Image 
