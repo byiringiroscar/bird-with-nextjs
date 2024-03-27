7ccc75b533f9189f6125280cb3317203cf760678
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-27 13:03:54 +0200

    wip on main

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -7,7 +7,8 @@ const MainContent = () => {
   return (
     <div className='w-full relative flex flex-col items-center py-8'>
 
-        <p className='border text-[11px] tracking-[0.1px] font-semibold text-[#858585] px-7 py-[5px] rounded-[43px] border-solid border-[#b1aeae] cursor-pointer'>Organization</p>
+        <p className='border text-[11px] tracking-[0.1px] font-semibold text-[#858585] px-7 py-[5px] rounded-[43px] border-solid border-[#b1aeae] cursor-pointer opensans-font-regular'>Organization</p>
+        <h1 className='text-center text-[41px] leading-[50px] mt-[25px] mb-2.5 pt-0 pb-3 px-0 montserrat-700'><span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>Evaluate Candidates</span> Quickly, <br /> Affordably, and Accurately</h1>
 
 
 
